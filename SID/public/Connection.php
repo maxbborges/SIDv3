e7d03d00cc5e7d73c1c998d9a27c4e9b162bc094
Maxwell Borges <max15borges@gmail.com>
2017-12-20 02:46:35 +0000

    melhoria no acesso ao banco

--- SID/public/Connection.php
+++ SID/public/Connection.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = pg_connect ( "host=localhost port=5432 dbname=sidiiff2.0 user=postgres password=1234" );
+$conn = pg_connect ( "host=localhost port=5432 dbname=sidiiff2 user=postgres password=1234" );
