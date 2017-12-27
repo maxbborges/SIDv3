31a6155088bf3f568067ca9d62ad6b49ece2e2f9
Maxwell Borges <max15borges@gmail.com>
2017-12-27 01:15:23 -0200

    Edicao modulo cliente

--- SID/public/js/cliente.js
+++ SID/public/js/cliente.js
@@ -2,20 +2,28 @@ var vetorLegenda = new Array();
 var vetorLegendaMarquee = new Array();
 var vetorLinkQr = new Array();
 var vetorImagens = new Array();
+var vetorImgPerfil = new Array();
+var vetorNomes  = new Array();
+var vetorComentarios  = new Array();
+var vetorInformacoes  = new Array();
 
 var contador = 0;
 
 $(document).ready(function(){
 	listarImagens();
 
-	// Divulgação Fixa 
+	// Divulgação Fixa
 	vetorLegenda.push("<marquee id='marquee' behavior='scroll' direction='left' scrollamount='30'><p>Conheça mais sobre o SID acessando o código QR ao lado</p></marquee>");
 	$("#linkQr").append("<img style='display: none;' class='imgQR' id='linkQr0' src='qrcode.php?link=https://dsnnunes.com.br/SID/'></img>");
 	vetorLinkQr.push("linkQr0");
 	$("#slide").append("<img style='display: none;' id='"+"imagem0' height='100%' width='100%' src='img/fixa.jpeg' alt='Imagem fixa' ></img>");
 	vetorImagens.push("imagem0");
+	vetorImgPerfil.push("perfil0");
+	vetorNomes.push("nomes0");
+	vetorComentarios.push("comentarios0");
+	vetorInformacoes.push("informacoes0");
 
-	setInterval(slide, 65000);
+	setInterval(slide, 5000);
 });
 
 function slide(){
@@ -31,21 +39,29 @@ function slide(){
 function divulgacao(anterior, atual){
 	//retirar atuais.
 	$('#divLegenda').empty();
-	
+
 	$("#"+vetorImagens[anterior]).css("display","none");
 	$("#"+vetorLinkQr[anterior]).css("display","none");
-	
+	$("#"+vetorImgPerfil[anterior]).css("display","none");
+	$("#"+vetorComentarios[anterior]).css("display","none");
+	$("#"+vetorNomes[anterior]).css("display","none");
+	$("#"+vetorInformacoes[anterior]).css("display","none");
+
 	//colocar novas
 	$('#divLegenda').append(vetorLegenda[atual]);
 	$('#'+vetorImagens[atual]).css("display", "block");
 	$('#'+vetorLinkQr[atual]).css("display", "block");
+	$('#'+vetorImgPerfil[atual]).css("display","block");
+	$('#'+vetorComentarios[atual]).css("display","block");
+	$('#'+vetorNomes[atual]).css("display","block");
+	$('#'+vetorInformacoes[atual]).css("display","block");
 }
 
 function listarImagens(){
 	$.ajax({
 		url: "cliente.php",
 		type: "POST",
-		dataType: "json", 
+		dataType: "json",
 		success: function(dados){
 			for (var i = 0;i < dados.length; i++) {
 				var id = dados[i].divid;
@@ -54,13 +70,31 @@ function listarImagens(){
 
 				$("#linkQr").append("<img style='display: none;' class='imgQR' id='linkQr"+id+"' src='qrcode.php?link="+dados[i].linkqr+"'></img>");
 				vetorLinkQr.push("linkQr"+id);
-				
-				$("#slide").append("<img style='display: none;' id='"+"imagem"+id+"' height='100%' width='100%' src='imagem.php?divid="+id+"' alt='Imagem Alt "+id+"' ></img>");
+
+				$("#slide").append("<img style='display: none;' id='"+"imagem"+id+"' height='100%' width='100%' src='/imagens/430736790662138.png' alt='Imagem Alt "+id+"' ></img>");
 				vetorImagens.push("imagem"+id);
+
+				// $("#slide").append("<img style='display: none;' id='"+"imagem"+id+"' height='100%' width='100%' src='imagem.php?divid="+id+"' alt='Imagem Alt "+id+"' ></img>");
+				// vetorImagens.push("imagem"+id);
+
+				$("#imgPerfilComent").append("<img style='display: none;' id='"+"perfil"+id+"' height='100%' width='100%' src='img/fixa.jpeg' alt='Imagem Alt "+id+"' ></img>");
+				vetorImgPerfil.push("perfil"+id);
+
+				$("#nomeComent").append("<div  style='display: none;' id='"+"nomes"+id+"'>NOME</div");
+				vetorNomes.push("nomes"+id);
+
+				$("#textComent").append("<div style='display: none;' id='"+"comentarios"+id+"'>Comentarios</div>");
+				vetorComentarios.push("comentarios"+id);
+
+				$("#infoComent").append("<div style='display: none;' id='"+"informacoes"+id+"'>Informacoes</div>");
+				vetorInformacoes.push("informacoes"+id);
+
+				// $("#textComent").append("<img style='display: none;' id='"+"comentario"+id+"' height='100%' width='100%' src='https://scontent.fbsb1-1.fna.fbcdn.net/v/t1.0-9/19702411_1290395001069450_5097736575801189523_n.jpg?oh=e7d1ef703e0fb3e41f3796e4c2daffe3&oe=5AB6689A' alt='Imagem Alt "+id+"' ></img>");
+				// vetorComentario.push("comentario"+id);
 
 			}
 		}, error:function(dados){
-			alert("Erro ao listar imagens!");
+			// alert("Erro ao listar imagens!");
 		}
 	});
 }
@@ -84,7 +118,7 @@ function date()
 	dia= today.getDate();
 	mes= today.getMonth();;
 	ano= today.getFullYear();
-	
+
 	if(mes=='0'){
 		mes="Janeiro";
 	}else if(mes=='1'){
