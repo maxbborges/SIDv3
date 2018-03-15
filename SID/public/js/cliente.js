b55122203d7c989f16ef08c03ba8f8e18795568d
Maxwell <max15borges@gmail.com>
2018-03-15 16:36:22 -0300

    Melhoria no modulo cliente.

--- SID/public/js/cliente.js
+++ SID/public/js/cliente.js
@@ -1,60 +1,76 @@
 var posicao=0; // Define Qual publicação será exibida, a publicação zero é a publicação Fixa.
-var intervaloPublicacaos=5000; // Define os intervalos entre publicações
+var intervaloPublicacoes=5000; // Define os intervalos entre publicações
+var intervaloRequisicao=10000;
 
 $(document).ready(function(){
+
+	// Define o intervalo de atualização dos comentarios/publicaçoes (Mude a valor de intervaloRequisicao)
 	setInterval(function(){
 		$.ajax({
-			url: "http://"+location.host+"/bd/",
+			url: "http://"+location.host+"/bd",
 			type: "GET",
-			data: {id: posicao},
 			dataType: "json",
 			success: function(dados){
-				// Limpa todos os campos de comentario para que seja exibido um novo comentario.
-				$("#imgPerfilComent").empty();
-				$("#textComent").empty();
-				$("#infoComent").empty();
-				$("#nomeComent").empty();
-				$('#divLegenda').empty();
-				$('#slide').empty();
-				$('#linkQr').empty();
 
-				// Apresenta na tela o QRCODE, a imagem e a legenda da publicação. Dados salvos no banco de dados.
-				$("#linkQr").append("<img class='imgQR' id='linkQrx' src='qrcode.php?link="+dados['BD']['linkqr']+"'></img>");
-				$("#slide").append("<img id='imagemx' height='100%' width='100%' src='data:image/png;base64,"+dados['imagem']+"' alt='Imagem Alt 1' ></img>");
-				$('#divLegenda').append("<marquee id='marquee' behavior='scroll' direction='left' scrollamount='30'><p>"+dados['BD']['legenda']+"</p></marquee>");
+				// Define o intervalo de troca das postagens (Mude intervaloPublicacoes)
+				const intervalo = setInterval(function(){
+					// Limpa todos os campos de comentario para que seja exibido um novo comentario.
+					$("#imgPerfilComent").empty();
+					$("#textComent").empty();
+					$("#infoComent").empty();
+					$("#nomeComent").empty();
+					$('#divLegenda').empty();
+					$('#slide').empty();
+					$('#linkQr').empty();
 
-				// Apresenta informaçoes como: Texto, foto de perfil, nome e data das publicaçoes.
-				// Caso não seja encontrado nenhum comentario ele apresenta o else.
-				if(dados['comentario']!=null){
-					// Recupera a data e hora da publicação que foi armazenada no Array dados.
-					var data = new Date(dados['comentario']['datas']).toLocaleDateString();
-					var hora = new Date(dados['comentario']['datas']).toLocaleTimeString();
+					// Apresenta na tela o QRCODE, a imagem e a legenda da publicação. Dados salvos no banco de dados.
+					$("#linkQr").append("<img class='imgQR' id='linkQrx' src='qrcode.php?link="+dados[posicao]['bd']['linkqr']+"'></img>");
+					$("#slide").append("<img id='imagemx' height='100%' width='100%' src='data:image/png;base64,"+dados[posicao]['imagem']+"' alt='Imagem Alt 1' ></img>");
+					$('#divLegenda').append("<marquee id='marquee' behavior='scroll' direction='left' scrollamount='30'><p>"+dados[posicao]['bd']['legenda']+"</p></marquee>");
 
-					//Recupera Imagem, comentario, nome e informaçoes da publicação que foi armazenada no Array dados.
-					$("#imgPerfilComent").append("<img id='perfil' height='100%' width='100%' src="+dados['comentario']['fotoPerfil']+" alt='Imagem Alt VALOR' ></img>");
-					$("#nomeComent").append("<div id='nomes'>"+dados['comentario']['nome']+"</div");
-					$("#textComent").append("<div>"+dados['comentario']['mensagem']+"</div>");
-					$("#infoComent").append("<br><div>Postado em: "+data+" as "+hora+"</div>");
-				} else {
-					if (posicao==0){
-						$("#nomeComent").append("<br><div>Este é o SID</div");
+					// Apresenta informaçoes como: Texto, foto de perfil, nome e data das publicaçoes.
+					// Caso não seja encontrado nenhum comentario ele apresenta o else.
+					if(dados[posicao]['comentarios']!=null){
+
+						// Gera um numero aleatorio para qual comentario será exibido.
+						var posicaoComentario = Math.floor(Math.random() * (((dados[posicao]['comentarios']).length) - 0) + 0);
+
+						// Recupera a data e hora da publicação que foi armazenada no Array dados.
+						var data = new Date(dados[posicao]['comentarios'][posicaoComentario]['created_time']).toLocaleDateString();
+						var hora = new Date(dados[posicao]['comentarios'][posicaoComentario]['created_time']).toLocaleTimeString();
+
+						//Recupera Imagem, comentario, nome e informaçoes da publicação que foi armazenada no Array dados.
+						$("#imgPerfilComent").append("<img id='perfil' height='100%' width='100%' src="+dados[posicao]['comentarios'][posicaoComentario]['urlFoto']+" alt='Imagem Alt VALOR' ></img>");
+						$("#nomeComent").append("<div id='nomes'>"+dados[posicao]['comentarios'][posicaoComentario]['from']['name']+"</div");
+						$("#textComent").append("<div>"+dados[posicao]['comentarios'][posicaoComentario]['message']+"</div>");
+						$("#infoComent").append("<br><div>Postado em: "+data+" as "+hora+"</div>");
 					} else {
-						$("#nomeComent").append("<br><div>Seja o primeiro a comentar nessa postagem!</div");
+						if (posicao==0){
+							$("#nomeComent").append("<br><div>Este é o SID</div");
+						} else {
+							$("#nomeComent").append("<br><div>Seja o primeiro a comentar nessa postagem!</div");
+						}
 					}
-				}
 
-				// Incrementa na posição para exibir a proxima publicação.
-				posicao++;
+					// Incrementa na posição para exibir a proxima publicação.
+					posicao++;
 
-				// Se o total de publicações inseridas no banco for atigida, o numero de posição é zerado, exibindo a publicação inicial.
-				if(posicao==dados['quantidade']){
-					posicao=0;
-				}
+					// Se o total de publicações inseridas no banco for atigida, o numero de posição é zerado, exibindo a publicação inicial.
+					if(posicao==dados.length){
+						posicao=0;
+					}
+				},intervaloPublicacoes);
+
+				// Zera o contador de tempo.
+				setInterval(function(){
+					clearInterval(intervalo);
+				},intervaloRequisicao);
+
 			}, error:function(dados){
 				alert("Erro! \nCodigo de erro: "+response.status);
 			}
 		});
-	},intervaloPublicacaos);
+	},intervaloRequisicao);
 });
 
 function time(){
