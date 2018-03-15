4ced2580b27d0a80b3f79a2ac1cb777858233882
Maxwell <max15borges@gmail.com>
2018-03-15 16:38:18 -0300

    Fim de exibicao por datas e restricoes de comentario no modulo cliente.

--- SID/module/Api/src/Api/Controller/RestfulbdController.php
+++ SID/module/Api/src/Api/Controller/RestfulbdController.php
@@ -10,96 +10,69 @@ class RestfulbdController extends AbstractRestfulController
 {
 
   public function get($id){
-    // Faz a conexão com o banco, requisita somente a linha especifica e armazena na variavel $linhaBanco
-    require_once 'public/Connection.php';
-    $sql = "select * from divulgacao order by divid";
-    $res = pg_exec($conn, $sql);
-    while($linha = pg_fetch_array($res,$row = NULL, $result_type = PGSQL_ASSOC)){
-      $vetor[] = array_map('htmlentities', $linha);
-    }
-
-    $linhaBanco = $vetor[$id];
-
-    // Requisita os arquivos de configuração da pagina e do Facebook.
-    if ($id != 0){
-      require_once (__DIR__.'/Configure1.php');
-      $configure = new Configure();
-      $infPagina = $configure->infPagina();
-      $fb = new \Facebook\Facebook($configure->newFacebook());
-
-      // Requisita todos os comentarios da publicação.
-      $comentariosPublicacao = ($fb->get('/'.$linhaBanco['object_id'].'/comments', $infPagina['tokenPagina']))->getDecodedBody();
-
-      if ($comentariosPublicacao['data']!= null){
-        $comentario=rand (0,((count($comentariosPublicacao['data']))-1));
-
-        // Recupera as informaçoes especificas do comentario.
-        $uid = $comentariosPublicacao['data'][$comentario]['from']['id'];
-        $urlFoto = ($fb->get('/'.$uid.'/?fields=picture.type(large)', $infPagina['tokenPagina']))->getDecodedBody();
-
-        $infPostagem = array(
-          'fotoPerfil' => $urlFoto['picture']['data']['url'],
-          'nome' => $comentariosPublicacao['data'][$comentario]['from']['name'],
-          'mensagem' => $comentariosPublicacao['data'][$comentario]['message'],
-          'datas' => $comentariosPublicacao['data'][$comentario]['created_time'],
-        );
-      }
-
-    }
-
-    // Cria um Array com as informaçoes
-    $json = array(
-      'comentario' => $infPostagem,
-      'imagem' => [base64_encode(file_get_contents("./public/imagens/".$linhaBanco['object_id'].".png"))],
-      'BD' => $linhaBanco,
-      'quantidade' => count($vetor),
-      'posicao' => $novaPosicao
-    );
-
-    // Transforma o Array em JSON
-    $json = json_encode($json);
-    $response = $this->getResponseWithHeader()->setContent($json);
+    $response = $this->getResponseWithHeader()->setContent(__METHOD__.'ira buscar id='.$id);
     return $response;
   }
 
   public function getList(){
-    $posicao = 1;
+    require_once (__DIR__.'/Configure1.php');
+    $configure = new Configure();
+    $infPagina = $configure->infPagina();
+
+    // Realiza a conexão com o banco e pega todas as colunas do banco.
     require_once 'public/Connection.php';
     $sql = "select * from divulgacao order by divid";
     $res = pg_exec($conn, $sql);
 
+    // Faz a leitura de cada linha recuperada do banco
     while($linha = pg_fetch_array($res,$row = NULL, $result_type = PGSQL_ASSOC)){
-      // Verifica se a o DIVID não é zero, pois a posicao 0 é destinada a publicação fixa, não possuindo Comentarios.
-      if ($linha['divid']!=0){
-        // Requisita os paramentros de conexão com o facebook e faz a conexão.
-        require_once (__DIR__.'/Configure1.php');
-        $configure = new Configure();
-        $infPagina = $configure->infPagina();
-        $fb = new \Facebook\Facebook($configure->newFacebook());
 
-        // Recupera todos os comentarios da publicação, usando o object_id de cada uma.
-        $comentarios = ($fb->get('/'.$linha['object_id'].'/comments', $infPagina['tokenPagina']))->getDecodedBody();
+      // Verifica a data de termino e de inicio de cada linha recuperada do banco.
+      // Caso a linha esteja fora do prazo, ela é ignorada.
+      if(date('Y-m-d')<$linha['datatermino']){
 
-        // Verifica se a publicação possui Comentarios.
-        if($comentarios['data']!=null){
-          // precorre todo todas as informaçoes dos comentarios e coloca a foto do usuario q publicou em cada uma delas.
+        // Verifica se a o DIVID não é zero.
+        // A posicao 0 é destinada a publicação fixa, não possuindo Comentarios.
+        if ($linha['divid']!=0){
+
+          // Requisita os paramentros de conexão com o facebook e faz a conexão.
+          $fb = new \Facebook\Facebook($configure->newFacebook());
+
+          // Recupera todos os comentarios da publicação, usando o object_id de cada uma.
+          $comentarios = ($fb->get('/'.$linha['object_id'].'/comments', $infPagina['tokenPagina']))->getDecodedBody();
+
+          // Percorre todos os comentarios recuperados.
           for($i=0; $i<count($comentarios['data']); $i++){
-            $urlFoto = ($fb->get("/".$comentarios['data'][$i]['from']['id']."/?fields=picture.type(large)", $infPagina['tokenPagina']))->getDecodedBody();
-            $url['urlFoto'] = $urlFoto['picture']['data']['url'];
-            $arrayComentarios[$i+1] = array_merge($comentarios['data'][$i], $url);
+
+            // Recupera todas os likes da publicação
+            $likes = ($fb->get('/'.$comentarios['data'][$i]['id'].'/likes', $infPagina['tokenPagina']))->getDecodedBody();
+
+            // Percorre todos os likes feitos na publicação
+            for($cont=0;$cont<count($likes);$cont++){
+
+              // Verifica se a publicação possui o like do administrador.
+              // Caso não possua, a publicação não é exibida.
+              if($likes['data'][$cont]['id']=="1371436046298678"){
+                $urlFoto = ($fb->get("/".$comentarios['data'][$i]['from']['id']."/?fields=picture.type(large)", $infPagina['tokenPagina']))->getDecodedBody();
+                $url['urlFoto'] = $urlFoto['picture']['data']['url'];
+                $arrayComentarios[] = array_merge($comentarios['data'][$i], $url);
+              }
+            }
           }
         }
-      }
 
-      $json[$posicao] = array(
-        'bd' => $linha,
-        'comentarios' => $arrayComentarios,
-      );
-      $arrayComentarios=null;
-      $posicao++;
+        // Cria um Array com as informaçoes recuperadas.
+        $json[] = array(
+          'bd' => $linha, // Informaçoes do Banco
+          'comentarios' => $arrayComentarios, // Informaçoes do Face
+          'imagem' => base64_encode(file_get_contents($infPagina['destinoLocal'].$linha['object_id'].".png")), // base64 da imagem armazenada localmente
+        );
+
+        $arrayComentarios=null;
+      }
     }
 
-    $json = json_encode($json);
+    $json = json_encode($json); // Transforma o Array em JSON
     $response = $this->getResponseWithHeader()->setContent($json);
     return $response;
   }
@@ -122,7 +95,6 @@ class RestfulbdController extends AbstractRestfulController
     return $response;
   }
 
-  // configure response
   public function getResponseWithHeader(){
     $response = $this->getResponse();
     $response->getHeaders()
